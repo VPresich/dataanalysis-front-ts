5617abcfc87c20619c792b294d9c5e5d310698c3
VPresich <valya.alexandrova.presich@gmail.com>
2025-11-29 08:53:19 +0100

    Change project config for migration on typescript

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { store, persistor } from "./redux/store";
 import App from "./components/App";
 import "./assets/css/index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
